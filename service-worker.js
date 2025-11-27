c33a75f60ca3c8eddbd24dee7f37f968fd98fa19
williandlima <117686973+williandlima@users.noreply.github.com>
2025-11-27 15:12:56 -0300

    Add basic service worker with install, activate, and fetch

---
+++ service-worker.js
@@ -0,0 +1,11 @@
+self.addEventListener("install", () => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", () => {
+  clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(fetch(event.request));
+});
